782c4ebfba3b32122e76a14ddf0a1b9e5ff27f08
Pramod Tiwari <pram.tiwa@gmail.com>
2018-01-13 16:53:01 +0530

    added comments for month and day

--- Date Util1.py
+++ Date Util1.py
@@ -2,7 +2,11 @@
 
 # a function that will return the current year.
 
-# a function that will return the timestamp
+# a function that will return the timestamp.
 
 # a function that will return the current month.
+
+# a function that will return me the current day of the week (sunday - saturday).
+
+
 
